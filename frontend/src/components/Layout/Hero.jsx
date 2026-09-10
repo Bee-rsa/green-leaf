7538fb609fa8a5bc7665a868b2c37d05703dcc13
Brendan <cleavermedia@outlook.com>
2026-09-10 19:04:14 +0200

    Merge existing GitHub repository

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
   }, []); 
  
   return ( 
-    <section className="flex flex-col md:flex-row h-[660px] sm:h-[702.5px] md:h-[600px] overflow-hidden"> 
+    <section className="flex flex-col md:flex-row h-[630px] sm:h-[672.5px] md:h-[600px] overflow-hidden"> 
  
       {/* Left - Image */} 
       <div 
